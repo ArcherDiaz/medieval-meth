743e3c7f2fbd7b33920993c003b43e811de09aee
MeshaMakes <meshamakesthings@gmail.com>
2025-04-03 20:49:08 -0300

    added gravity to main character

--- MainCharacter/main_character.gd
+++ MainCharacter/main_character.gd
@@ -5,8 +5,8 @@ const JUMP_VELOCITY = -1000.0
 const GRAVITY = 4000
 
 var holdYData = Vector2.ZERO
-var attackAnims = ["attack1", "attack2", "attack3"]
-var isAttacking
+var attckAnims = ["attack1", "attack2","attack3"]
+var is_attacking
 var randAttack
 
 func get_input():
@@ -19,26 +19,26 @@ func get_input():
 		velocity.x -= SPEED
 		$AnimatedSprite2D.play("run")
 		$AnimatedSprite2D.flip_h = true
-	elif Input.is_action_just_pressed("attack"):
-		isAttacking = true
-		randAttack = attackAnims[randi() % attackAnims.size()]
-	elif isAttacking == true:
+	elif Input.is_action_pressed("attack"):
+		is_attacking = true
+		randAttack = attckAnims[randi() % attckAnims.size()]
+	elif is_attacking == true:
 		$AnimatedSprite2D.play(randAttack)
 	else:
 		$AnimatedSprite2D.play("idle")
-		
 
 func _physics_process(delta):
 	get_input()
+	velocity.y += GRAVITY * delta
 	if Input.is_action_just_pressed("ui_up"):
 		if is_on_floor():
 			velocity.y = JUMP_VELOCITY
 			$AnimatedSprite2D.play("jump")
 	elif not is_on_floor() and velocity.y > JUMP_VELOCITY - -800:
 		$AnimatedSprite2D.play("fall")
-
+	
 	move_and_slide()
 
 
 func _on_animated_sprite_2d_animation_finished():
-	isAttacking = false
+	is_attacking=false
