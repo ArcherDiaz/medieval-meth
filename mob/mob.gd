2ebf50e5e9b394cd49a8733409dc1e643fe3741c
Jaycoder <jerganhector@gmail.com>
2025-04-03 20:16:01 -0400

    added gravity to the mob

--- mob/mob.gd
+++ mob/mob.gd
@@ -1,15 +1,20 @@
 extends CharacterBody2D
 
-var speed = 50
-var gravity = 1
+var speed = 300
+var gravity = 500
 var direction = Vector2.RIGHT 
 
 func _ready():
 	$AnimatedSprite2D.play()
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
-	queue_free()
+	$AnimatedSprite2D.flip_h = true
+	direction = Vector2.LEFT
+	#queue_free()
 	
 func _physics_process(delta):
-	velocity = direction * speed 
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	velocity.x = direction.x * speed
 	move_and_slide()
